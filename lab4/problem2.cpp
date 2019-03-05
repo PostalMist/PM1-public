9a8d54e9f73b161e6d5d564b28d306e5222b553a
Reme <31744912+PostalMist@users.noreply.github.com>
2019-03-04 23:05:23 -0500

    Update problem2.cpp

--- lab4/problem2.cpp
+++ lab4/problem2.cpp
@@ -4,7 +4,29 @@
 // else return -1 
 int binarySearch(char* arr[], char* x, int n) 
 { 
-    //your code here 
+     int l = 0 ; 
+        int r = n - 1; 
+        while (l <= r)  
+        { 
+            int m = l + (r - l) / 2; 
+  
+        int res; 
+        if (x == (arr[m])) 
+            res = 0; 
+              
+  
+            // Check if x is present at mid 
+            if (res == 0) 
+                return m; 
+  
+            // If x greater, ignore left half 
+            if (x > (arr[m])) 
+                l = m + 1; 
+  
+            // If x is smaller, ignore right half 
+            else
+                r = m - 1; 
+        } 
 
     return -1; 
 } 
